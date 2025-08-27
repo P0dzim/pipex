639129ff61b444babab3402636caa807aa8cef36
vitosant <vitor.200483@gmail.com>
2025-08-27 11:30:56 -0300

    put write_error.c file in the norm

--- src/write_error.c
+++ src/write_error.c
@@ -6,14 +6,14 @@
 /*   By: vitosant <vitosant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 11:21:08 by vitosant          #+#    #+#             */
-/*   Updated: 2025/08/27 11:22:10 by vitosant         ###   ########.fr       */
+/*   Updated: 2025/08/27 11:25:54 by vitosant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	write_error(int return_value)
+void	write_error(int return_pid)
 {
 	perror(strerror(errno));
-	exit(return_value);
+	exit(return_pid);
 }
